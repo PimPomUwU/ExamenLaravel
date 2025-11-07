cef754280d909b4d7a0ebe039ad5b88c95cb206b
PimPomUwU <daviparralopez@gmail.com>
2025-11-07 17:39:45 -0500

    Rol no sope

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -14,41 +14,10 @@ class Rol extends Model
         'name',
     ];
 
-    protected $allowIncluded = [
-        'users',
-    ];
-
-
-    // Campos por los que se puede ordenar
-    protected $allowSort = [
-        'id',
-        'name',
-
-    ];
 
     //relaciones
     public function users () {
        return $this->belongsToMany(User::class);
-    }
-
-            public function scopeIncluded(Builder $query)
-    {
-        if (empty($this->allowIncluded) || empty(request("included"))) {
-            return;
-        }
-
-        $relations = explode(',', request('included'));
-
-        $allowIncluded = collect($this->allowIncluded);
-
-        foreach ($relations as $key => $relationship) {
-
-            if (!$allowIncluded->contains($relationship)) {
-                unset($relations[$key]);
-            }
-        }
-
-        $query->with($relations);
     }
 
 }
